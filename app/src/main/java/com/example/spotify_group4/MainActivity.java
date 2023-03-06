2f044a424569c84e5e3abfefb8adbe9c51c4bcca
VanThanhHoang <HoangVanThanhDev@gmail.com>
2023-03-06 20:17:12 +0700

    đổi theme thành material3

--- app/src/main/java/com/example/spotify_group4/MainActivity.java
+++ app/src/main/java/com/example/spotify_group4/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.spotify_group4;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
