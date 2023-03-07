44fb015acc55aabe56a99d4725da4806ebe70787
VanThanhHoang <HoangVanThanhDev@gmail.com>
2023-03-07 23:32:15 +0700

    hết ngày 3/7

---
+++ app/src/main/java/com/example/spotify_group4/View/Activity/LoginActivity.java
@@ -0,0 +1,40 @@
+package com.example.spotify_group4.View.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.spotify_group4.Listener.ReplaceFragmentListener;
+import com.example.spotify_group4.R;
+import com.example.spotify_group4.View.Fragment.LoginFragment;
+import com.example.spotify_group4.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity implements ReplaceFragmentListener {
+    ActivityLoginBinding layoutBinding;
+    LoginFragment loginFragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        layoutBinding = ActivityLoginBinding.inflate(getLayoutInflater(), null, false);
+        setContentView(layoutBinding.getRoot());
+        layoutBinding.topAppBar.setNavigationOnClickListener(v-> onBackPressed());
+        initFragment();
+        replaceFragment(loginFragment);
+    }
+    private void initFragment() {
+        loginFragment = new LoginFragment();
+    }
+    @Override
+    public void replaceFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment);
+        if(fragment instanceof LoginFragment){
+            fragmentTransaction.commit();
+            return;
+        }
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+}
