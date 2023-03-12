72405d13df9ab5e7a07d51311e410211ec3ecdea
VanThanhHoang <HoangVanThanhDev@gmail.com>
2023-03-12 13:07:24 +0700

    update seekbar

--- app/src/main/java/com/example/spotify_group4/View/Fragment/MusicPlayFragment.java
+++ app/src/main/java/com/example/spotify_group4/View/Fragment/MusicPlayFragment.java
@@ -5,6 +5,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
