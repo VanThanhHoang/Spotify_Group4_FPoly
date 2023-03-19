bba6686761fb07ba24eaf605c350baf4b13b9edc
VanThanhHoang <HoangVanThanhDev@gmail.com>
2023-03-19 23:33:13 +0700

    silder, list

--- app/src/main/java/com/example/spotify_group4/View/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/spotify_group4/View/Fragment/HomeFragment.java
@@ -7,9 +7,7 @@ import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,13 +15,16 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.spotify_group4.Adapter.HomeSliderAdapter;
 import com.example.spotify_group4.Adapter.PlaylistAdapter;
+import com.example.spotify_group4.Helper.AnimationZoomViewPager;
 import com.example.spotify_group4.Listener.ReplaceFragmentListener;
 import com.example.spotify_group4.Model.HomeContent;
 import com.example.spotify_group4.Model.PlayList;
 import com.example.spotify_group4.Retrofit.ApiSkyMusic;
 import com.example.spotify_group4.databinding.FragmentHomeBinding;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,16 +35,13 @@ import retrofit2.Response;
 
 public class HomeFragment extends Fragment {
     FragmentHomeBinding layoutBinding;
-    List<PlayList> playLists;
+    List<PlayList> playListSlider;
     RecyclerView[] listDefaultPlayList;
     TextView[] listDefaultContentTittle;
     ReplaceFragmentListener replaceFragmentListener;
     List<HomeContent> homeContents;
-
-    void createPlayListRecycleView() {
-
-    }
-
+    Handler handler ;
+    Runnable sliderRunnable;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -52,24 +50,28 @@ public class HomeFragment extends Fragment {
     }
 
 
-    void createPlayLists() {
-        playLists = new ArrayList<>();
-        Call<List<PlayList>> callGetPlayList = ApiSkyMusic.apiSkyMusic.getAllPlayList();
-        callGetPlayList.enqueue(new Callback<List<PlayList>>() {
+    void hideShimmer() {
+        layoutBinding.shimmerLayout.hideShimmer();
+        if (layoutBinding.layoutRefresh.isRefreshing()) {
+            layoutBinding.layoutRefresh.setRefreshing(false);
+        }
+        layoutBinding.shimmerLayout.setVisibility(View.INVISIBLE);
+    }
+
+    void getPlayListSlider() {
+        playListSlider = new ArrayList<>();
+        Call<List<PlayList>> callGetSliderPlayLists = ApiSkyMusic.apiSkyMusic.getSliderPlayList();
+        callGetSliderPlayLists.enqueue(new Callback<List<PlayList>>() {
             @Override
             public void onResponse(@NonNull Call<List<PlayList>> call, @NonNull Response<List<PlayList>> response) {
-                layoutBinding.shimmerLayout.hideShimmer();
-                if (layoutBinding.layoutRefresh.isRefreshing()) {
-                    layoutBinding.layoutRefresh.setRefreshing(false);
-                }
-                layoutBinding.shimmerLayout.setVisibility(View.INVISIBLE);
-                playLists = response.body();
-                createPlayListRecycleView();
+                playListSlider = response.body();
+                createSlider();
+                autoTransSlider();
             }
 
             @Override
             public void onFailure(@NonNull Call<List<PlayList>> call, @NonNull Throwable t) {
-                Toast.makeText(getContext(), "Sever bận !", Toast.LENGTH_SHORT).show();
+
             }
         });
     }
@@ -80,6 +82,7 @@ public class HomeFragment extends Fragment {
         callGetHomeContent.enqueue(new Callback<List<HomeContent>>() {
             @Override
             public void onResponse(@NonNull Call<List<HomeContent>> call, @NonNull Response<List<HomeContent>> response) {
+                hideShimmer();
                 homeContents = response.body();
                 createTittleContent();
                 createRecycleViewPlaylist();
@@ -92,31 +95,41 @@ public class HomeFragment extends Fragment {
         });
     }
 
+    void createSlider() {
+        assert getActivity() != null;
+        HomeSliderAdapter homeSliderAdapter = new HomeSliderAdapter(getActivity(), playListSlider);
+        layoutBinding.vpgHomeSlider.setAdapter(homeSliderAdapter);
+        layoutBinding.vpgHomeSlider.setPageTransformer(new AnimationZoomViewPager());
+        layoutBinding.homeCircleIndicatior.setViewPager(layoutBinding.vpgHomeSlider);
+        homeSliderAdapter.registerAdapterDataObserver(layoutBinding.homeCircleIndicatior.getAdapterDataObserver());
+    }
+
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
         replaceFragmentListener = (ReplaceFragmentListener) getContext();
     }
 
-    void createRecycleView() {
 
-    }
-    void createTittleContent(){
-        for(int i = 0;i<listDefaultContentTittle.length;i++){
+    void createTittleContent() {
+        for (int i = 0; i < listDefaultContentTittle.length; i++) {
             listDefaultContentTittle[i].setText(homeContents.get(i).getName());
         }
     }
-    void createRecycleViewPlaylist(){
-        for(int i = 0;i<listDefaultContentTittle.length;i++){
-            PlaylistAdapter adapter = new PlaylistAdapter(homeContents.get(i).getPlayLists(),replaceFragmentListener);
-            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false);
+
+    void createRecycleViewPlaylist() {
+        for (int i = 0; i < listDefaultContentTittle.length; i++) {
+            PlaylistAdapter adapter = new PlaylistAdapter(homeContents.get(i).getPlayLists(), replaceFragmentListener);
+            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
             listDefaultPlayList[i].setLayoutManager(linearLayoutManager);
             listDefaultPlayList[i].setAdapter(adapter);
         }
     }
+
     void initEvent() {
         layoutBinding.layoutRefresh.setOnRefreshListener(() -> {
-            createPlayLists();
+            getHomeContent();
+            getPlayListSlider();
             new Handler(Looper.getMainLooper()).postDelayed(() -> {
                 if (layoutBinding.layoutRefresh.isRefreshing()) {
                     layoutBinding.layoutRefresh.setRefreshing(false);
@@ -125,11 +138,46 @@ public class HomeFragment extends Fragment {
         });
     }
 
+    private void autoTransSlider() {
+        handler = new Handler(Looper.getMainLooper());
+        sliderRunnable = () -> {
+            int currentPos = layoutBinding.vpgHomeSlider.getCurrentItem();
+            int total = playListSlider.size() - 1;
+            if (currentPos < total) {
+                currentPos++;
+                layoutBinding.vpgHomeSlider.setCurrentItem(currentPos);
+            } else {
+                layoutBinding.vpgHomeSlider.setCurrentItem(0);
+            }
+            handler.postDelayed(sliderRunnable,3000);
+        };
+        sliderRunnable.run();
+    }
+    void setTextHello(){
+        LocalTime currentTime = LocalTime.now();
+        int hour = currentTime.getHour();
+        if (hour >= 4 && hour < 12) {
+            layoutBinding.tvHelloHomeFragment.setText("Chào buổi sáng!");
+        } else if (hour >= 12 && hour < 15) {
+            layoutBinding.tvHelloHomeFragment.setText("Buổi trưa vui vẻ nhé !");
+        } else if (hour >= 15 && hour < 18) {
+            layoutBinding.tvHelloHomeFragment.setText("Chúc buổi chiều đầy năng động!");
+        } else {
+            layoutBinding.tvHelloHomeFragment.setText("Buổi tối tốt lành !");
+        }
+    }
+    @Override
+    public void onDestroy() {
+
+        super.onDestroy();
+    }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        setTextHello();
         layoutBinding.shimmerLayout.startShimmer();
-        createPlayLists();
         getHomeContent();
+        getPlayListSlider();
         initEvent();
         listDefaultPlayList = new RecyclerView[]{
                 layoutBinding.rvPlayListDefault0,
@@ -147,7 +195,6 @@ public class HomeFragment extends Fragment {
                 layoutBinding.tvContentTittle4,
                 layoutBinding.tvContentTittle5
         };
-        createRecycleView();
         replaceFragmentListener.showComponents();
         super.onViewCreated(view, savedInstanceState);
     }
---
+++ app/src/main/java/com/example/spotify_group4/View/Fragment/HomeSliderFragment.java
@@ -0,0 +1,48 @@
+package com.example.spotify_group4.View.Fragment;
+
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.spotify_group4.Model.PlayList;
+import com.example.spotify_group4.databinding.LayoutHomeSliderBinding;
+import com.squareup.picasso.Picasso;
+
+public class HomeSliderFragment extends Fragment {
+    PlayList playList;
+    LayoutHomeSliderBinding layoutHomeSliderBinding;
+
+    public HomeSliderFragment(PlayList playList) {
+        this.playList = playList;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        layoutHomeSliderBinding = LayoutHomeSliderBinding.inflate(getLayoutInflater(), container, false);
+        return layoutHomeSliderBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        layoutHomeSliderBinding.tvPlayListName.setText(playList.getName());
+        Picasso.get().load(playList.getUrlImg()).into(layoutHomeSliderBinding.imgSlider);
+        setColorMatrix(layoutHomeSliderBinding.imgSlider);
+        super.onViewCreated(view, savedInstanceState);
+    }
+    void setColorMatrix(ImageView imgPlayList){
+        ColorMatrix colorMatrix = new ColorMatrix();
+        colorMatrix.setSaturation(0); // 0 means grayscale
+        colorMatrix.setScale(0.8f, 0.8f, 0.7f, 1); // Scale all color channels by 0.5
+        ColorMatrixColorFilter colorFilter = new ColorMatrixColorFilter(colorMatrix);
+        imgPlayList.setColorFilter(colorFilter);
+    }
+}
