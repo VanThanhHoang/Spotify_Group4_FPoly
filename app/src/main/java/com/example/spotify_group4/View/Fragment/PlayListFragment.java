2bc838b072b5bc6f36ac1c531712956324a22ae4
dawngphi <phongphi273@gmail.com>
2023-03-26 12:51:56 +0700

    custom playlist

--- app/src/main/java/com/example/spotify_group4/View/Fragment/PlayListFragment.java
+++ app/src/main/java/com/example/spotify_group4/View/Fragment/PlayListFragment.java
@@ -1,5 +1,7 @@
 package com.example.spotify_group4.View.Fragment;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,7 +10,9 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.palette.graphics.Palette;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -17,8 +21,10 @@ import com.example.spotify_group4.Adapter.SongAdapter;
 import com.example.spotify_group4.Listener.GetSongListListener;
 import com.example.spotify_group4.Listener.ReplaceFragmentListener;
 import com.example.spotify_group4.Model.Song;
+import com.example.spotify_group4.R;
 import com.example.spotify_group4.Presenter.PlayListFragmentPresenter;
 import com.example.spotify_group4.databinding.FragmentPlaylistBinding;
+import com.google.android.material.appbar.AppBarLayout;
 
 import java.util.List;
 
@@ -26,6 +32,8 @@ public class PlayListFragment extends Fragment implements GetSongListListener {
     FragmentPlaylistBinding fragmentPlaylistBinding;
     List<Song> mSongList;
     SongAdapter songAdapter;
+    private boolean isExpanded = true;
+    private boolean isPlaying = false;
     int idPlayList;
     PlayListFragmentPresenter playListFragmentPresenter;
 
@@ -38,6 +46,18 @@ public class PlayListFragment extends Fragment implements GetSongListListener {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         fragmentPlaylistBinding = FragmentPlaylistBinding.inflate(getLayoutInflater(), container, false);
+        initToolbar();
+        initToolbarAnimation();
+        fragmentPlaylistBinding.playPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isPlaying) {
+                    pause();
+                } else {
+                    play();
+                }
+            }
+        });
         return fragmentPlaylistBinding.getRoot();
     }
 
@@ -79,4 +99,45 @@ public class PlayListFragment extends Fragment implements GetSongListListener {
     public void onGetSongListFail() {
 
     }
+    private void initToolbar(){
+        ((AppCompatActivity) getActivity()).setSupportActionBar(fragmentPlaylistBinding.toolbar);
+        if (((AppCompatActivity) getActivity()).getSupportActionBar() != null){
+            ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+    private void initToolbarAnimation(){
+        fragmentPlaylistBinding.collapsingToolbarLayout.setTitle(getString(R.string.app_name));
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.son_tung);
+        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+            @Override
+            public void onGenerated(@Nullable Palette palette) {
+                // setmàu toolbar
+                int myColor = palette.getVibrantColor(getResources().getColor(R.color.green));
+                fragmentPlaylistBinding.collapsingToolbarLayout.setContentScrimColor(myColor);
+                fragmentPlaylistBinding.collapsingToolbarLayout.setStatusBarScrimColor(getResources().getColor(R.color.gray_contain));
+            }
+        });
+        fragmentPlaylistBinding.appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (Math.abs(verticalOffset)>200){
+                    isExpanded = false;
+                }else {
+                    isExpanded = true;
+                }
+            }
+        });
+    }
+    private void play() {
+        isPlaying = true;
+        fragmentPlaylistBinding.playPauseButton.setImageResource(R.drawable.ic_pause);
+        // TODO: Start playing audio or video.
+    }
+
+    private void pause() {
+        isPlaying = false;
+        fragmentPlaylistBinding.playPauseButton.setImageResource(R.drawable.ic_play);
+        // TODO: Pause playing audio or video.
+    }
+
 }
